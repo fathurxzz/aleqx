297f027dabaec5dbdb0bfbf0a47aa1df781cf2c1
kushko.alex@gmail.com <kushko.alex@gmail.com@db27b7b8-cc40-11dd-b6f5-21ad4398c266>
2012-02-22 09:23:09 +0000

    Добавлены теги

---
+++ trunk/Shop/Shop/Models/TagValidation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    [MetadataType(typeof(TagValidation))]
+    public partial class Tag
+    {
+         
+    }
+
+    public class TagValidation
+    {
+        [Required(ErrorMessage = "Обязательно!")]
+        [DisplayName("Название")]
+        public string Value { get; set; }
+    }
+}
