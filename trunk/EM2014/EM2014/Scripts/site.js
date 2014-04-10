30228e452b8fbe5f4b0d080d8c7af9a5ebf4fab8
kushko.alex@gmail.com <kushko.alex@gmail.com@db27b7b8-cc40-11dd-b6f5-21ad4398c266>
2014-04-10 15:42:16 +0000

    ajax

--- trunk/EM2014/EM2014/Scripts/site.js
+++ trunk/EM2014/EM2014/Scripts/site.js
@@ -15,4 +15,21 @@
         $("#logo").css("cursor", "pointer").click(function () { location.href = "/"; });
     }
 
+    $(".current-bg.current").css("display", "block");
+
 });
+
+
+function beginreq(obj) {
+    //$(obj).parent().addClass("current");
+
+    //obj).removeClass("current");
+    $(".current-bg").fadeOut();
+    //$("#productsContainer").fadeOut(200);
+}
+
+function endreq(obj) {
+    var id = "#" + $(obj).attr("id");
+    $(id + " > div.current-bg-container > div.current-bg").fadeIn();
+    //$("#productsContainer").fadeIn(200);
+}
