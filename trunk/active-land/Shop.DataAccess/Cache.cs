6f16e99f4cdc75b1b2d5ac814f66693d540e61bc
konstantin.khodykin <konstantin.khodykin@db27b7b8-cc40-11dd-b6f5-21ad4398c266>
2014-12-28 21:42:50 +0000

    zzz

---
+++ trunk/active-land/Shop.DataAccess/Cache.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace Shop.DataAccess
+{
+    public class Cache
+    {
+        public static readonly ConcurrentDictionary<string, object> Default = new ConcurrentDictionary<string, object>();
+    }
+}
